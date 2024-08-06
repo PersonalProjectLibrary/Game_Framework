bc0b279f3e59a6c7626973550bf7e813a399edf3
LoriaRujoy <1306650725@qq.com>
2024-08-06 21:27:29 +0800

    新建HotFix热更工程

---
+++ FrameStudy/HotFixProject/HotFix/HotFix/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace HotFix
+{
+    public class TestClass
+    {
+        public static void StaticFunTest()
+        {
+            Debug.Log("TestClass StaticFunTest!!!!!!!!!!");
+        }
+    }
+}
