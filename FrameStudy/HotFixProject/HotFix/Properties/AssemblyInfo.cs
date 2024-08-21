56df96865d4924ce60023814d72677c1aab10346
LoriaRujoy <1306650725@qq.com>
2024-08-21 17:14:23 +0800

    Unity主工程里的脚本迁移到热更工程进行热更 迁移过程，出现问题： 放到热更工程中的脚本有类型无法识别找不到； 在工程分析文件夹Analysis_Project里：65_热更UIManager_Window类 进行了详细说明解决过程，并解决了程序集里的类型不识别问题。

--- FrameStudy/HotFixProject/HotFix/Properties/AssemblyInfo.cs
+++ FrameStudy/HotFixProject/HotFix/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("6bebf725-846f-4a9b-8482-7f9c37c52f46")]
+[assembly: Guid("8aa41103-780b-4d98-94a9-f3aaba113a31")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
