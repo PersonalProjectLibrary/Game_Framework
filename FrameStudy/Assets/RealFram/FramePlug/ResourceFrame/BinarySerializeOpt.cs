28e54014f606f90e613dcd9ad02e8adb2a26fa8b
LoriaRujoy <1306650725@qq.com>
2024-08-26 15:07:32 +0800

    添加两种protobuf序列化和反序列化方式 写在BinarySerializeOpt.cs里 一种是适用于类本地文件保存的序列化和反序列化方式； 一种是适用于网络数据传输的序列化与反序列化方式；

--- FrameStudy/Assets/RealFram/FramePlug/ResourceFrame/BinarySerializeOpt.cs
+++ FrameStudy/Assets/RealFram/FramePlug/ResourceFrame/BinarySerializeOpt.cs
@@ -3,9 +3,94 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
 using UnityEngine;
+using ProtoBuf;
 
 public class BinarySerializeOpt
 {
+    #region Protobuf
+    /************************适用于本地文件保存的Protobuf序列化和反序列化************************************/
+    /// <summary>
+    /// 最简单的将类序列化为文件
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static bool ProtoSerialize(string path,System.Object obj)
+    {
+        try
+        {
+            using (Stream file = File.Create(path))
+            {
+                Serializer.Serialize(file, obj);//将类存下来
+                return true;
+            }
+        }
+        catch (Exception e) 
+        { 
+            Debug.LogError(e);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Protobuf反序列化
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static T ProtoDeSerialize<T>(string path)where T : class
+    {
+        try
+        {
+            using (Stream file = File.OpenRead(path)) { return Serializer.Deserialize<T>(file); }
+        }
+        catch (Exception e) 
+        { 
+            Debug.LogError(e);
+            return null;
+        }
+    }
+
+    /************************适用于网络数据传输的Protobuf序列化和反序列化************************************/
+    public static byte[] ProtoSerialize(System.Object obj)
+    {
+        try
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Serializer.Serialize(ms, obj);
+                byte[] res = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(res, 0, res.Length);
+                return res;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+            return null;
+        }
+    }
+    public static T ProtoDeSerialize<T>(byte[] msg) where T : class
+    {
+        try
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ms.Write(msg, 0, msg.Length);
+                ms.Position = 0;
+                return Serializer.Deserialize<T>(ms);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+            return null;
+        }
+    }
+    #endregion
+
+    #region Xml
     /// <summary>
     /// 类序列化成xml
     /// </summary>
@@ -27,11 +112,9 @@ public class BinarySerializeOpt
                 }
             }
             return true;
-;        }
-        catch (Exception e)
-        {
-            Debug.LogError("此类无法转换成xml " + obj.GetType() + "," + e);
+            ;
         }
+        catch (Exception e) { Debug.LogError("此类无法转换成xml " + obj.GetType() + "," + e); }
         return false;
     }
 
@@ -52,10 +135,7 @@ public class BinarySerializeOpt
                 t = (T)xs.Deserialize(fs);
             }
         }
-        catch (Exception e)
-        {
-            Debug.LogError("此xml无法转成二进制: " + path + "," + e);
-        }
+        catch (Exception e) { Debug.LogError("此xml无法转成二进制: " + path + "," + e); }
         return t;
     }
 
@@ -76,13 +156,9 @@ public class BinarySerializeOpt
                 obj = xs.Deserialize(fs);
             }
         }
-        catch (Exception e)
-        {
-            Debug.LogError("此xml无法转成二进制: " + path + "," + e);
-        }
+        catch (Exception e) { Debug.LogError("此xml无法转成二进制: " + path + "," + e); }
         return obj;
     }
-
 
     /// <summary>
     /// 运行时使读取xml
@@ -100,7 +176,6 @@ public class BinarySerializeOpt
             UnityEngine.Debug.LogError("cant load TextAsset: " + path);
             return null;
         }
-
         try
         {
             using (MemoryStream stream = new MemoryStream(textAsset.bytes))
@@ -110,13 +185,12 @@ public class BinarySerializeOpt
             }
             ResourceManager.Instance.ReleaseResouce(path, true);
         }
-        catch (Exception e)
-        {
-            Debug.LogError("load TextAsset exception: " + path + "," + e);
-        }
+        catch (Exception e) { Debug.LogError("load TextAsset exception: " + path + "," + e); }
         return t;
     }
+    #endregion
 
+    #region Binary
     /// <summary>
     /// 类转换成二进制
     /// </summary>
@@ -134,10 +208,7 @@ public class BinarySerializeOpt
             }
             return true;
         }
-        catch (Exception e)
-        {
-            Debug.LogError("此类无法转换成二进制 " + obj.GetType() + "," + e);
-        }
+        catch (Exception e) { Debug.LogError("此类无法转换成二进制 " + obj.GetType() + "," + e); }
         return false;
     }
 
@@ -157,7 +228,6 @@ public class BinarySerializeOpt
             UnityEngine.Debug.LogError("cant load TextAsset: " + path);
             return null;
         }
-
         try
         {
             using (MemoryStream stream = new MemoryStream(textAsset.bytes))
@@ -167,10 +237,8 @@ public class BinarySerializeOpt
             }
             ResourceManager.Instance.ReleaseResouce(path, true);
         }
-        catch (Exception e)
-        {
-            Debug.LogError("load TextAsset exception: " + path + "," + e);
-        }
+        catch (Exception e) { Debug.LogError("load TextAsset exception: " + path + "," + e); }
         return t;
     }
+    #endregion
 }
